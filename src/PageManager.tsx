dtonon/fevela

commit e3e44aea9491c46c21532332f4ec884ea83b8597

miscellaneous changes from jumble november git history.

src/PageManager.tsx
@@ -433,8 +433,9 @@ export function PageManager({ maxStackSize = 5 }: { maxStackSize?: number }) {
                 <Sidebar />
                 <div
                   className={cn(
-                    'grid grid-cols-2 w-full',
-                    themeSetting === 'pure-black' ? '' : 'gap-2 pr-2 py-2'
+                    'bg-background overflow-hidden',
+                    themeSetting === 'pure-black' ? 'border-l' : 'rounded-lg',
+                    themeSetting !== 'pure-black' && secondaryStack.length > 0 && 'shadow-lg'
                   )}
                 >
                   <div

index.html
@@ -12,7 +12,6 @@
     />
 
     <meta name="apple-mobile-web-app-title" content="Fevela" />
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
     <link rel="icon" href="/favicon.ico" sizes="48x48" />
     <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
     <meta name="theme-color" content="#171717" media="(prefers-color-scheme: dark)" />

src/components/ReplyNoteList/index.tsx
@@ -5,6 +5,7 @@ import {
   getParentTag,
   getRootTag,
   isMentioningMutedUsers,
+  isProtectedEvent,
   isReplyNoteEvent
 } from '@/lib/event'
 import { toNote } from '@/lib/link'
@@ -143,10 +144,15 @@ export default function ReplyNoteList({
           })
       }
 
+      if (isProtectedEvent(event)) {
+        const seenOn = client.getSeenEventRelayUrls(event.id)
+        relays.push(...seenOn)
+      }
+
       setSubRequests(
         filters.map((filter) => ({
           source: 'relays',
-          urls: relays.concat(BIG_RELAY_URLS).slice(0, 5),
+          urls: relays.concat(BIG_RELAY_URLS).slice(0, 8),
           filter
         }))
       )

src/index.css
@@ -8,6 +8,11 @@
     -webkit-tap-highlight-color: transparent;
   }
 
+  body,
+  html {
+    overscroll-behavior-y: none;
+  }
+
   html {
     @apply font-sans antialiased;
     --bc-color-brand: hsl(var(--primary));

commit 93fd9ef6572279109706885ee39f40c0cc7b1b6e

fix dual panel.
had been broken by e3e44aea9491c46c21532332f4ec884ea83b8597

src/PageManager.tsx
@@ -433,8 +433,8 @@ export function PageManager({ maxStackSize = 5 }: { maxStackSize?: number }) {
                 <Sidebar />
                 <div
                   className={cn(
-                    'bg-background overflow-hidden',
-                    themeSetting === 'pure-black' ? 'border-l' : 'rounded-lg',
+                    'grid grid-cols-2 w-full bg-background overflow-hidden',
+                    themeSetting === 'pure-black' ? 'border-l' : 'gap-2 pr-2 py-2 rounded-lg',
                     themeSetting !== 'pure-black' && secondaryStack.length > 0 && 'shadow-lg'
                   )}
                 >