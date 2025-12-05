0eb286230df54b70d19281c2b031cc4d8a280b65
fiatjaf <fiatjaf@gmail.com>
2025-12-05 16:43:28 -0300

    enable forced ping and subscription reconnections at the @nostr/tools level.
    hopefully this makes it so syncing doesn't stop forever when internet is temporarily off or whatever.
    
    thanks @chr15m.

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,7 @@ setVh()
 initNostrWasm()
   .then((nw) => {
     setNostrWasm(nw)
-    setPool(new AbstractSimplePool({ verifyEvent }))
+    setPool(new AbstractSimplePool({ verifyEvent, enablePing: true, enableReconnect: true }))
     pool.trackRelays = true
 
     createRoot(document.getElementById('root')!).render(
